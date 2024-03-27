4e1262c454b37fe6060dba12acd4680a95a49589
Evelyn Hill <evelynplumhill@gmail.com>
2024-03-27 13:27:32 -0400

    Basic implementation of image to ascii

--- main.c
+++ main.c
@@ -1,18 +1,58 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 int main() {
 
-  FILE *pF = fopen("amogus.png", "r");
-  FILE *pW = fopen("output.txt", "w");
+  FILE *pF = fopen("output.txt", "w");
 
-  char fileOut[10000];
+  int width, height, channels;
 
-  while (fgets(fileOut, 10000, pF) != NULL) {
-    fprintf(pW, "%s", fileOut);
+  unsigned char *image = stbi_load("amogus.png", &width, &height, &channels, 0);
+
+  if (image == NULL) {
+    printf("Error loading image!\n");
   }
 
+  printf("Loaded image with width: %d height: %d and channels: %d\n", width,
+         height, channels);
+
+  size_t image_size = width * height * channels;
+  int i;
+  for (unsigned char *p = image; p != image + image_size; p += channels) {
+    int brightness = 0;
+
+    brightness = *p + *(p + 1) + *(p + 2) / 3.0;
+
+    if (brightness < 85) {
+      fprintf(pF, "@");
+    } else if (brightness > 85 && brightness < 170) {
+      fprintf(pF, "$");
+    } else if (brightness > 170 && brightness < 255) {
+      fprintf(pF, "*");
+    } else if (brightness > 255 && brightness < 340) {
+      fprintf(pF, "/");
+    } else if (brightness > 340 && brightness < 425) {
+      fprintf(pF, "^");
+    } else if (brightness > 425 && brightness < 510) {
+      fprintf(pF, ".");
+    } else if (brightness > 510) {
+      fprintf(pF, " ");
+    }
+    i++;
+    if (i >= width) {
+      fprintf(pF, "\n");
+      i = 0;
+    }
+  }
+
+  printf("Image data written to file.");
+
+  stbi_image_free(image);
   fclose(pF);
-  fclose(pW);
 
   return 0;
 }
